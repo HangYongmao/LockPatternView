a5d47de86ad539c63b4b5a28a3b29892819ace56
HangYongmao <1981217256@qq.com>
2018-02-08 00:57:11 +0800

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# LockPatternView
-九宫格图案锁
+## LockPatternView
+## 九宫格图案锁
+![](https://github.com/1139618418/LockPatternView/blob/master/art/LockPattern.gif)
+## Features
+* 小于4个点 代表错误 会红色高亮提示 2秒后移除 也可以手动重输
+* 连接过得点不能再次连接
+* 途径中间的点会自动连接（横、竖、斜的方向）
---
+++ app/src/main/java/com/example/lockpatternview/main_activity.java
@@ -0,0 +1,13 @@
+package com.example.lockpatternview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_lockpattern);
+    }
+}
